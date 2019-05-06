806416dca45c868bed284cce1b357a9ab3a7cf5b
Irshad Ansari <irshadjsr21@gmail.com>
2019-05-06 17:15:53 +0530

    [Feat] Made the header responsive

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,33 +1,77 @@
-import React from "react"
+import React, { Component } from "react"
 import { Link } from "gatsby"
-import { FaLinkedin, FaTwitter, FaEnvelope } from "react-icons/fa"
+import {
+  FaLinkedin,
+  FaTwitter,
+  FaEnvelope,
+  FaBars,
+  FaTimes,
+} from "react-icons/fa"
 import styles from "../styles/header.module.scss"
 import logo from "../images/Logo.png"
 
-export default () => (
-  <nav className={styles.container}>
-    <div className={styles.linkContainer}>
-      <Link to="/">Home</Link>
-      <Link to="/">About</Link>
-      <Link to="/">Skills</Link>
-      <Link to="/">Project</Link>
-      <Link to="/">Contact</Link>
-    </div>
-    <div className={styles.logoConatiner}>
-      <Link to="/">
-        <img src={logo} alt="Irshad logo" />
-      </Link>
-    </div>
-    <div className={styles.iconContainer}>
-      <Link to="/">
-        <FaLinkedin className={styles.icon} />
-      </Link>
-      <Link to="/">
-        <FaTwitter className={styles.icon} />
-      </Link>
-      <Link to="/">
-        <FaEnvelope className={styles.icon} />
-      </Link>
-    </div>
-  </nav>
-)
+export default class header extends Component {
+  render() {
+    return (
+      <nav className={styles.container}>
+        <div className={styles.linkContainer}>
+          <input
+            type="checkbox"
+            name="nav-toggle"
+            id="nav-toggle"
+            className={styles.navToggleInput}
+          />
+          <div className={styles.navToggleContainer}>
+            <label htmlFor="nav-toggle">
+              <FaBars className={styles.navMenuIcon + " clickable"} />
+            </label>
+          </div>
+          <div className={styles.links}>
+            <label htmlFor="nav-toggle">
+              <FaTimes className={styles.navCloseIcon + " clickable"} />
+            </label>
+            <div className={styles.link + " clickable"}>
+              <div className={styles.linkBg} />
+              <Link to="/">Home</Link>
+            </div>
+            <div className={styles.link + " clickable"}>
+              <div className={styles.linkBg} />
+              <Link to="/">About</Link>
+            </div>
+            <div className={styles.link + " clickable"}>
+              <div className={styles.linkBg} />
+              <Link to="/">Skills</Link>
+            </div>
+            <div className={styles.link + " clickable"}>
+              <div className={styles.linkBg} />
+              <Link to="/">Project</Link>
+            </div>
+            <div className={styles.link + " clickable"}>
+              <div className={styles.linkBg} />
+              <Link to="/">Contact</Link>
+            </div>
+          </div>
+        </div>
+        <div className={styles.logoConatiner}>
+          <Link to="/">
+            <img src={logo} alt="Irshad logo" />
+          </Link>
+        </div>
+        <div className={styles.iconContainer}>
+          <a
+            href="https://www.linkedin.com/in/irshad-ansari-aa094216b/"
+            target="_black"
+          >
+            <FaLinkedin className={styles.icon} />
+          </a>
+          <a href="https://twitter.com/irshadansari7" target="_black">
+            <FaTwitter className={styles.icon} />
+          </a>
+          <a href="mailto:irshadjsr21@gmail.com" target="_black">
+            <FaEnvelope className={styles.icon} />
+          </a>
+        </div>
+      </nav>
+    )
+  }
+}
