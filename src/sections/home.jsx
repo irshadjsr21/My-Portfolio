3b5ea67fd09ed6dd2c5190d62c8115a288946d70
Irshad Ansari <irshadjsr21@gmail.com>
2019-05-08 19:11:11 +0530

    [Feat] Added animation; Changed image path

--- src/sections/home.jsx
+++ src/sections/home.jsx
@@ -1,4 +1,5 @@
 import React from "react"
+import ScrollAnimation from "react-animate-on-scroll"
 import styles from "../styles/home.module.scss"
 import illustration from "../images/onboarding.svg"
 import { getTextContents } from "../services/home"
@@ -10,13 +11,40 @@ export default function Home() {
       <div className={styles.mainContent}>
         <div className={styles.textContainer}>
           <div className={styles.greeting + " font-serif"}>
-            <h2 className={styles.hi}>{data.headerText1}</h2>
-            <h1 className={styles.name}>{data.headerText2}</h1>
+            <ScrollAnimation
+              animateIn="fadeInLeft"
+              duration={0.8}
+              animateOnce={true}
+            >
+              <h2 className={styles.hi}>{data.headerText1}</h2>
+            </ScrollAnimation>
+            <ScrollAnimation
+              animateIn="fadeInLeft"
+              duration={0.8}
+              animateOnce={true}
+              delay={500}
+            >
+              <h1 className={styles.name}>{data.headerText2}</h1>
+            </ScrollAnimation>
           </div>
-          <div className={styles.introText}>{data.introText}</div>
+          <ScrollAnimation
+            animateIn="fadeInLeft"
+            duration={0.8}
+            animateOnce={true}
+            delay={1000}
+          >
+            <div className={styles.introText}>{data.introText}</div>
+          </ScrollAnimation>
         </div>
         <div className={styles.illustration}>
-          <img src={illustration} alt="Onboarding illustration" />
+          <ScrollAnimation
+            animateIn="fadeInRight"
+            duration={0.8}
+            animateOnce={true}
+            delay={1500}
+          >
+            <img src={illustration} alt="Onboarding illustration" />
+          </ScrollAnimation>
         </div>
       </div>
     </section>
